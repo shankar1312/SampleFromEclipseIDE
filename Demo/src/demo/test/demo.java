c022756cf4429dc74b86a86a3403e00d7ee4a9f1
Shankar <Shankar@192.168.0.110>
2021-05-31 22:05:20 +0530

    This is my first commit.

---
+++ Demo/src/demo/test/demo.java
@@ -0,0 +1,11 @@
+package demo.test;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Shankar";
+		System.out.println(name);
+	}
+
+}
