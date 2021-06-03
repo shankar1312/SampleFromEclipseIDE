87e107ec716fff4f116d5b6da0a9ca21ac1886dc
Shankar <shankar1312@gmail.com>
2021-06-03 15:40:58 +0530

    update

--- Demo/src/com/learn/accessmodifier/pack1/SamePkgSubCls.java
+++ Demo/src/com/learn/accessmodifier/pack1/SamePkgSubCls.java
@@ -5,6 +5,8 @@ public class SamePkgSubCls extends SamePkgSameCls{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//using inheritance concept
+		
 		SamePkgSubCls obj = new SamePkgSubCls();
 		
 		System.out.println(obj.publicVariable);
